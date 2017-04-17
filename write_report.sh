137aad293a45411d75847e554681f013ea6bfd4c
Bruno Costa <netbofia@hotmail.com>
2017-04-17 15:50:00 +0100

    Global log sed error

--- write_report.sh
+++ write_report.sh
@@ -199,45 +199,45 @@ if [[ "${TYPE}" == "logs" ]]; then
 	LOGS=${workdir}log
 	#Choose last dir. 
 	lastlog=$(ls -d ${LOGS}/*/ | tail -1)
-	if [[ -e $(ls ${LOGS}/*Global*  | tail -1) ]]; then
+	if [[ -e $(ls "${LOGS}"/*Global*  | tail -1) ]]; then
 		printf "\\\section{Global log}\n" >> ${OUTPUT_FILE}
-		sed -r "s:$:\\\\\\\:g" ${log} | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
+		sed -r "s:$:\\\\\\\:g" "${LOGS}"/*Global* | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
 	fi
 	if [[ -e $(echo "${lastlog}"/*pipe_fastq*) ]]; then
 		printf "\\\section{Pipe Fastq}\n" >> ${OUTPUT_FILE}
 		log=$(echo "${lastlog}"/*pipe_fastq*)
-		sed -r "s:$:\\\\\\\:g" ${log} | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
+		sed -r "s:$:\\\\\\\:g" "${log}" | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
 	fi
 	if [[ -e $(echo ${lastlog}/*pipe_fasta*) ]]; then
 		printf "\\\section{Pipe Fasta}\n" >> ${OUTPUT_FILE}
 		log=$(echo "${lastlog}"/*pipe_fasta*)
-		sed -r "s:$:\\\\\\\:g" ${log} | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
+		sed -r "s:$:\\\\\\\:g" "${log}" | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
 	fi
 	if [[ -e $(echo "${lastlog}"/*filters*) ]]; then
 		printf "\\\section{Filtering}\n" >> ${OUTPUT_FILE}
 		log=$(echo "${lastlog}"/*filters*)
-		sed -r "s:$:\\\\\\\:g" ${log} | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
+		sed -r "s:$:\\\\\\\:g" "${log}" | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
 	fi
 	if [[ -e $(echo "${lastlog}"/*genome_*_mirbase*) ]]; then
 		printf "\\\section{Genome and Mirbase Filtering}\n" >> ${OUTPUT_FILE}
 		log=$(echo "${lastlog}"/*genome*)
-		sed -r "s:$:\\\\\\\:g" ${log} | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
+		sed -r "s:$:\\\\\\\:g" "${log}" | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
 	fi
 	if [[ -e $(echo ${lastlog}/*mircat*) ]]; then
 		printf "\\\section{Mircat}\n" >> ${OUTPUT_FILE}
 		log=$(echo "${lastlog}"/*mircat*)
-		sed -r "s:$:\\\\\\\:g" ${log} | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
+		sed -r "s:$:\\\\\\\:g" "${log}" | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
 	fi
 	if [[ -e $(echo "${lastlog}"/*tasi*) ]]; then
 		printf "\\\section{TaSi}\n" >> ${OUTPUT_FILE}
 		log=$(echo "${lastlog}"/*tasi*)
-		sed -r "s:$:\\\\\\\:g" ${log} | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
+		sed -r "s:$:\\\\\\\:g" "${log}" | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
 	fi
 	#Extra for another log.
 	if [[ -e $(echo "${lastlog}"/*tasi---*) ]]; then
 		printf "\\\section{TaSi}\n" >> ${OUTPUT_FILE}
 		log=$(echo "${lastlog}"/*tasi*)
-		sed -r "s:$:\\\\\\\:g" ${log} | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
+		sed -r "s:$:\\\\\\\:g" "${log}" | sed -r "s:([#_&]):\\\\\1:g" >> ${OUTPUT_FILE}
 	fi
 	##Are there others?
 
