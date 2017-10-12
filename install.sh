529e6b7d75aff2cc844c23ee79816c8cd572e008
Bruno Costa <netbofia@hotmail.com>
2017-10-12 16:39:38 +0100

    Fix install var for commit sha

--- install.sh
+++ install.sh
@@ -100,8 +100,8 @@ else
   echo "   git clone https://github.com/forestbiotech-lab/miRPrusuit"
   installJSON=".install_time_stateOFart.json"
   curl https://api.github.com/repos/forestbiotech-lab/miRPursuit/commits > $installJSON
-  commit_SHA=$(grep -m1 sha $installJSON | sed -r "s:[\" ,]::g" | awk -F ":" '{print $2}')
-  sed -ri "s:(GIT=)(.*):\1${commit_sha}:" ${CFG}
+  commitSHA=$(grep -m1 sha $installJSON | sed -r "s:[\" ,]::g" | awk -F ":" '{print $2}')
+  sed -ri "s:(GIT=)(.*):\1${commitSHA}:" ${CFG}
   sleep 5
 fi
 
