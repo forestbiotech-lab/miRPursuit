a11a7241b46cb3f74ebde523feceefde95dad3b2
Bruno Costa <netbofia@hotmail.com>
2017-11-15 12:07:18 +0000

    Added trap to catch errors

--- scripts/fq_to_fa_exe.sh
+++ scripts/fq_to_fa_exe.sh
@@ -9,15 +9,22 @@
 # call fq_to_fa_exe.sh [work_dir] [lib_no] [file op]
 
 # define input directory and get input filename(s)
-set -e
 
-work_dir=${1}
-fastq_dir=${work_dir}"data/fastq"
-fasta_dir=${work_dir}"data/fasta"
-qual_dir=${work_dir}"data/quality"
+
+err_report() {
+   >&2 echo "Error -  on line $1 caused a code $2 exit - $3"
+   echo "Error -  on line $1 caused a code $2 exit - $3"
+}
+trap 'err_report $LINENO $? $(basename $0)' ERR
+
+
+work_dir=$1
+fastq_dir=${work_dir}"/data/fastq"
+fasta_dir=${work_dir}"/data/fasta"
+qual_dir=${work_dir}"/data/quality"
 mkdir -p ${qual_dir}
 mkdir -p ${fasta_dir}
-lib=$(printf "%02d\n" ${2})
+lib=$(printf "%02d\n" $2)
 
 files=$(ls ${fastq_dir}"/Lib"${lib}*".fq")
 
@@ -27,13 +34,13 @@ do
 	# print file treated now
 	echo "fastq_to_fasta "$file_now
 	# define outputfile
-  basename_file_now=$(basename ${file_now})
+  	basename_file_now=$(basename ${file_now})
 	out=$(echo $basename_file_now | awk '{gsub("fq","fa"); print}')
 	# perform fastqc
 	fastq_to_fasta -Q33 -i ${file_now} -o ${fasta_dir}"/"${out}
 	#Quality scores
 	fastx_quality_stats -Q33 -i ${file_now} -o ${qual_dir}"/lib"${lib}".stat"
-	/usr/local/bin/fastq_quality_boxplot_graph.sh -i ${qual_dir}"/lib"${lib}".stat" -t lib${lib} -p -o ${qual_dir}"/lib"${lib}".pdf"
+	fastq_quality_boxplot_graph.sh -i ${qual_dir}"/lib"${lib}".stat" -t lib${lib} -p -o ${qual_dir}"/lib"${lib}".pdf"
 
 	echo "done"
 done
