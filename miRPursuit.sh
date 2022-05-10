446efc648438384d034409e8656417d9e9e9cc1c
Bruno Costa <netbofia@hotmail.com>
2022-05-10 19:26:41 +0100

    Fix termination for no file found

--- miRPursuit.sh
+++ miRPursuit.sh
@@ -355,7 +355,7 @@ if [[ -d "${INSERTS_DIR}" && "${step}" == "0" ]]; then
 else
   if [[ -d "${INSERTS_DIR}" ]]; then
     echo -e "${grey}sRNA directory (INSERTS_DIR)${brown}=${NC} ${green}${INSERTS_DIR}${NC}"
-    if [[ ${INSERTS_DIR} == "\e[0;31mNOT FOUND\e[0m" ]];then 
+    if [[ ${testLib} == "\e[0;31mNOT FOUND\e[0m" ]];then 
       echo -e "${red}==> Common string${NC}: The string used to group the sRNAs hasn't produced a proper result, see ${blue}https://mirpursuit.readthedocs.io/en/latest/gettingstarted.html#how-to-run-the-program${NC} ."
       exit 127
     fi
