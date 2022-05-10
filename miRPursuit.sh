e72a57931431063aeaffb40231a465d783df12de
Bruno Costa <netbofia@hotmail.com>
2022-05-10 18:42:35 +0100

    Fix first file example for fasta. Detects error and shows

--- miRPursuit.sh
+++ miRPursuit.sh
@@ -355,7 +355,7 @@ if [[ -d "${INSERTS_DIR}" && "${step}" == "0" ]]; then
 else
   if [[ -d "${INSERTS_DIR}" ]]; then
     echo -e "${grey}sRNA directory (INSERTS_DIR)${brown}=${NC} ${green}${INSERTS_DIR}${NC}"
-    if [[ ${INSERTS_DIR} == "${red}NOT FOUND!!${NC}" ]];then 
+    if [[ ${INSERTS_DIR} == "NOT FOUND!!" ]];then 
       echo -e "${red}==> Common string${NC}: The string used to group the sRNAs hasn't produced a proper result, see ${blue}https://mirpursuit.readthedocs.io/en/latest/gettingstarted.html#how-to-run-the-program${NC} ."
       exit 127
     fi
